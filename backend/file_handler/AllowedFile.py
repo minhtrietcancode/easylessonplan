f368a382b9c318c85486cb0cb0d0c3deffbe0571
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-08-26 00:22:31 +1000

    rename + define config file for allowed file

---
+++ backend/file_handler/AllowedFile.py
@@ -0,0 +1,13 @@
+allowed_file = {
+    # pdf file 
+    "pdf" : {
+        "extractor_relative_path" : "backend/file_handler/all_file_types_handler/PdfExtractor.py",
+        "class_name" : "PdfExtractor"
+    },
+
+    # docx - ms word file
+    "docx" : {
+        "extractor_relative_path" : "backend/file_handler/all_file_types_handler/DocxExtractor.py",
+        "class_name" : "DocxExtractor"
+    }
+}
