7b8d4b9eafb0cb2eda3bbc46ed07e52976a5a3fe
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-08-25 23:40:18 +1000

    update pdf extractor, --> return dictionary with page as key

--- backend/file_handler/pdf_handler/pdf_extractor.py
+++ backend/file_handler/pdf_handler/pdf_extractor.py
@@ -2,21 +2,21 @@ from PyPDF2 import PdfReader
 import os
 
 class PdfHandler:
-    def extract_text_from_pdf(self, relative_path: str) -> str:
+    def extract_text_from_pdf(self, relative_path: str) -> dict:
         full_path = os.path.abspath(relative_path)
 
         if not os.path.exists(full_path):
             raise FileNotFoundError(f"File not found: {relative_path}")
 
         reader = PdfReader(full_path)
-        extracted_text = ""
+        extracted_text_by_page = {}
 
-        for page in reader.pages:
-            extracted_text += page.extract_text() or ""
+        for idx, page in enumerate(reader.pages, start=1):
+            extracted_text_by_page[idx] = page.extract_text() or ""
 
-        return extracted_text.strip()
+        return extracted_text_by_page
     
 if __name__ == "__main__":
     handler = PdfHandler()
-    text = handler.extract_text_from_pdf("test_file/pdf/module01-slides-print-5.pdf")
-    print(text)
+    text_by_page = handler.extract_text_from_pdf("test_file/pdf/module01-slides-print-5.pdf")
+    print(text_by_page)
