7717730525be33c438551ed5b9f7a8c76cfc5b96
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-08-26 00:37:18 +1000

    define procedure for FileHandler

--- backend/file_handler/FileHandler.py
+++ backend/file_handler/FileHandler.py
@@ -1,47 +1,20 @@
-
-import os
-
-class FileHandler:
-    def __init__(self, file_path: str):
-        self.file_path = file_path
-
-    def get_file_type(self) -> str:
-        _, file_extension = os.path.splitext(self.file_path)
-        return file_extension.lstrip('.').lower()
-
-    def validate_file_type(self) -> bool:
-        from AllowedFile import allowed_file
-        file_type = self.get_file_type()
-        if file_type in allowed_file:
-            return True
-        else:
-            print(f"Notification: File type '{file_type}' is not supported.")
-            return False
-
-    def extract_file_content(self) -> dict:
-        from AllowedFile import allowed_file
-        file_type = self.get_file_type()
-
-        if not self.validate_file_type():
-            return {}
-
-        handler_info = allowed_file[file_type]
-        handler_relative_path = handler_info["handler_relative_path"]
-        class_name = handler_info["class_name"]
-
-        # Dynamically import the handler module
-        module_path = handler_relative_path.replace("/", ".")[:-3]  # Remove .py
-        module = __import__(module_path, fromlist=[class_name])
-        ExtractorClass = getattr(module, class_name)
-
-        extractor = ExtractorClass()
-        return extractor.extract_text(self.file_path)
-    
 '''
 We need these things for the class FileHandler 
     - With instance variables 
         + FileHandler class should have an instance for each file type's extractor 
+        + An instance called: Extractor currentExtractor;
     
     - With the methods 
-        + 
+        + getFileType(file_path): --> this will return the type of that file 
+        + validateFileType(file_path): --> check if the type is in allowed file or not --> return True / False
+        + updateCurrentExtractor(file_path): this one would getFileType() first --> validateFileType() later
+            - if the validate function return False then print a statement to notify + exit this function / return False
+            - if the validate function return True, then follow this procedure 
+                + use the allowed_file imported from the AllowedFile class to look for correct 
+                  Extractor of the current file_path  --> then change the instance currentExtractor 
+                  to that correct Extractor
+        + extractText(file_path): 
+            - call the updateCurrentExtractor() function first, if it return False --> then exit no need to do anything
+            - if the function has done, then use the currentExtractor which has been updated above 
+              --> call the method currentExtractor.extractText(file_path)
 '''
