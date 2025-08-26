c3d35479ba9464e3e582d5eb6d7e5ecfe007a77a
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-08-26 20:16:34 +1000

    setup config file for backend/ - load all API key + config model + base url for llm models

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#################################################################################
+        # LOAD ALL API KEY + CONFIGURE MODEL AND URL FOR ALL LLM MODELS
+#################################################################################
+'''
+Firstly with the free Qwen model
+'''
+QWEN_BASE_URL = "https://openrouter.ai/api/v1"
+QWEN_MODEL = "qwen/qwen2.5-vl-32b-instruct:free"
+OPENROUTER_QWEN_API = os.getenv("OPENROUTER_QWEN_API")
+
+'''
+Secondly would be free Gemini model
+'''
