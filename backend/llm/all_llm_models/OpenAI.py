572d9607db6fd54ae0fcf9894ec54960bd57f13c
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-08-26 20:44:55 +1000

    setup gemini + openai class

---
+++ backend/llm/all_llm_models/OpenAI.py
@@ -0,0 +1,21 @@
+# avoid disrupting the code by import error, importing the config
+try:
+    from ...config import OPENAI_BASE_URL, OPENAI_MODEL, OPENROUTER_OPENAI_API
+except ImportError:
+    import sys
+    import os
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+    from config import OPENAI_BASE_URL, OPENAI_MODEL, OPENROUTER_OPENAI_API
+
+# avoid disrupting the code by import error, importing the base class
+try:
+    from .Model import Model
+except ImportError:
+    from Model import Model
+
+class OpenAI(Model):
+    def __init__(self):
+        super().__init__()
+        self.base_url = OPENAI_BASE_URL
+        self.model_name = OPENAI_MODEL
+        self.api_key = OPENROUTER_OPENAI_API
