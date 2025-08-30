5905106b913ad76abb734b4c72fd6495773076f0
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-08-30 16:44:45 +1000

    rename loginauth/auth.py -> LoginAuth.py

--- backend/loginauth/__init__.py
+++ backend/loginauth/__init__.py
@@ -1,4 +1,4 @@
-from .auth import auth_bp, login_required
+from .LoginAuth import auth_bp, login_required
 from .AuthConfig import AuthConfig
 
 __all__ = ['auth_bp', 'login_required', 'AuthConfig']
