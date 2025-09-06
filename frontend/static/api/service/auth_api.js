a2556aa93bca4a5078a02b5f7660888c943438ab
Minh Triet Pham <minhtrietp@student.unimelb.edu.au>
2025-09-06 12:59:48 +1000

    fix routes constant call - auth_api.js -> stupid error bruh

--- frontend/static/api/service/auth_api.js
+++ frontend/static/api/service/auth_api.js
@@ -27,6 +27,6 @@ export class AuthAPI {
      * Initiate login process
      */
     initiateLogin() {
-        window.location.href = window.API_ROUTES.GET_LOGIN;
+        window.location.href = window.API_ROUTES.AUTH.GET_LOGIN;
     }
 }
